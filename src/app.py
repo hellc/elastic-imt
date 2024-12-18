da842dfbf0dfe6a71b53257120df61a03ecb9f18
hellc <ihellc@outlook.com>
2024-12-18 22:51:09 +0300

    Simple migration task Project settings

--- src/app.py
+++ src/app.py
@@ -1,7 +1,12 @@
-import streamlit as st
-import elasticsearch
-from elasticsearch import helpers
 import asyncio
+import warnings
+
+import elasticsearch
+import streamlit as st
+import tqdm.asyncio as atqdm
+from elasticsearch import ElasticsearchWarning, helpers
+
+warnings.simplefilter("ignore", category=ElasticsearchWarning)
 
 # Elasticsearch settings
 st.sidebar.header("Elasticsearch Settings")
@@ -15,8 +20,6 @@ dst_es_hosts = st.sidebar.text_input(
 # Migration tasks
 st.header("Migration Tasks")
 tasks = []
-progress_bars = {}
-completed_tasks = []
 
 
 # Async migration function
@@ -27,9 +30,6 @@ async def migrate_index(task):
     dst_es = elasticsearch.AsyncElasticsearch(
         hosts=dst_es_hosts.split(","), headers={"Content-Type": "application/json"}
     )
-    progress_text = task["src_index"]
-    progress_bar = st.progress(0, text=progress_text)
-    progress_bars[task["src_index"]] = progress_bar
 
     # Get source index settings and mapping
     src_index_settings = await src_es.indices.get_settings(index=task["src_index"])
@@ -51,23 +51,30 @@ async def migrate_index(task):
         )
 
     # Get document count
-    src_index_count = await src_es.count(index=task["src_index"])
+    query = {"query": {"match_all": {}}}  # TODO dates filter
+    src_index_count = await src_es.count(index=task["src_index"], body=query)
     total_docs = src_index_count["count"]
 
     current_doc_enum = 0
-    async for doc in helpers.async_scan(
-        src_es, index=task["src_index"], query={"query": {"match_all": {}}}
-    ):
+
+    async_scan_iterator = helpers.async_scan(
+        src_es, index=task["src_index"], query=query
+    )
+    tqdm_bar = atqdm.tqdm(async_scan_iterator, total=total_docs)
+    async for doc in tqdm_bar:
         current_doc_enum += 1
         await dst_es.index(
             index=task["dst_index"], body=doc["_source"], id=doc.get("_id", None)
         )
-        progress_bar.progress(
+        task["progress_bar"].progress(
             current_doc_enum / total_docs, text=f"{current_doc_enum}/{total_docs}"
         )
+        tqdm_bar.desc = task["dst_index"]
 
-    progress_bar.empty()
-    completed_tasks.append(task)
+    task["progress_bar"].empty()
+    task["is_completed"] = True
+    await src_es.close()
+    await dst_es.close()
 
 
 async def main():
@@ -75,10 +82,17 @@ async def main():
     with st.form("add_task"):
         src_index = st.text_input("Source Index")
         dst_index = st.text_input("Destination Index")
+        progress_bar = st.progress(0, text=dst_index)
+
         submit_button = st.form_submit_button("Add Task")
 
         if submit_button:
-            task = {"src_index": src_index, "dst_index": dst_index}
+            task = {
+                "src_index": src_index,
+                "dst_index": dst_index,
+                "progress_bar": progress_bar,
+                "is_completed": False,
+            }
             tasks.append(task)
             await migrate_index(task)
 
